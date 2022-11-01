7c34cf9a26d83f579f2fcf7fbb8447560a521041
Ruben van Osch <soc@it-pg.nl>
2022-11-01 14:38:33 +0100

    Added github actions

--- SonicWallInterface/Program.cs
+++ SonicWallInterface/Program.cs
@@ -6,12 +6,23 @@ using SonicWallInterface.Configuration;
 using SonicWallInterface.Consumers;
 using SonicWallInterface.Services;
 using Messaging.Contracts;
+using System.Reflection;
 
 namespace SonicWallInterface
 {
     public class Program
     {
         public static async Task Main(string[] args)
+        {
+            if(args.Any(a => a == "--version" || a == "-v"))
+            {
+                Console.WriteLine($"v{Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion}");
+                return;
+            }
+            await Run(args);
+        }
+
+        private static async Task Run(string[] args)
         {
             var configurationBuilder = new ConfigurationBuilder()
                 .SetBasePath(Environment.CurrentDirectory)
@@ -22,63 +33,63 @@ namespace SonicWallInterface
             }
             var config = configurationBuilder.Build();
             await Host.CreateDefaultBuilder(args)
-                .ConfigureServices((context, services) =>
+            .ConfigureServices((context, services) =>
+            {
+                context.Configuration = config;
+                services.Configure<ServiceBusConfig>(context.Configuration.GetSection(nameof(ServiceBusConfig)));
+                services.Configure<SonicWallConfig>(context.Configuration.GetSection(nameof(SonicWallConfig)));
+                services.Configure<ThreatIntelApiConfig>(context.Configuration.GetSection(nameof(ThreatIntelApiConfig)));
+                
+                if(context.Configuration.GetSection(nameof(SonicWallConfig)).Get<SonicWallConfig>().IsPresent)
                 {
-                    context.Configuration = config;
-                    services.Configure<ServiceBusConfig>(context.Configuration.GetSection(nameof(ServiceBusConfig)));
-                    services.Configure<SonicWallConfig>(context.Configuration.GetSection(nameof(SonicWallConfig)));
-                    services.Configure<ThreatIntelApiConfig>(context.Configuration.GetSection(nameof(ThreatIntelApiConfig)));
-                    
-                    if(context.Configuration.GetSection(nameof(SonicWallConfig)).Get<SonicWallConfig>().IsPresent)
+                    services.AddSingleton<ISonicWallApi, SonicWallTIApi>();
+                }
+                else 
+                {
+                    throw new Exception("Missing configuration: SonicWallConfig");
+                }
+                var tiConfig = context.Configuration.GetSection(nameof(ThreatIntelApiConfig)).Get<ThreatIntelApiConfig>();
+                if(tiConfig.IsPresent && string.IsNullOrEmpty(tiConfig.WorkspaceId))
+                {
+                    services.AddSingleton<IThreatIntelApi, ThreatIntelApi>();
+                }
+                else if(tiConfig.IsPresent)
+                {
+                    services.AddSingleton<IThreatIntelApi, ThreatIntelLogAnalyticsApi>();
+                }
+                else 
+                {
+                    throw new Exception("Missing configuration: ThreatIntelApiConfig");
+                }
+                var serviceBusConfig = context.Configuration.GetSection(nameof(ServiceBusConfig)).Get<ServiceBusConfig>();
+                var appConfig = context.Configuration.GetSection(nameof(AppConfig)).Get<AppConfig>();
+                if(!serviceBusConfig.IsPresent){
+                    throw new Exception("Missing Messaging configuration");
+                }
+                services.AddMassTransit(x =>
+                {
+                    x.AddConsumer<BlockIPsConsumer>(typeof(BlockIPsConsumerDefinition));
+                    if (serviceBusConfig.IsPresent)
                     {
-                        services.AddSingleton<ISonicWallApi, SonicWallTIApi>();
-                    }
-                    else 
-                    {
-                        throw new Exception("Missing configuration: SonicWallConfig");
-                    }
-                    var tiConfig = context.Configuration.GetSection(nameof(ThreatIntelApiConfig)).Get<ThreatIntelApiConfig>();
-                    if(tiConfig.IsPresent && string.IsNullOrEmpty(tiConfig.WorkspaceId))
-                    {
-                        services.AddSingleton<IThreatIntelApi, ThreatIntelApi>();
-                    }
-                    else if(tiConfig.IsPresent)
-                    {
-                        services.AddSingleton<IThreatIntelApi, ThreatIntelLogAnalyticsApi>();
-                    }
-                    else 
-                    {
-                        throw new Exception("Missing configuration: ThreatIntelApiConfig");
-                    }
-                    var serviceBusConfig = context.Configuration.GetSection(nameof(ServiceBusConfig)).Get<ServiceBusConfig>();
-                    var appConfig = context.Configuration.GetSection(nameof(AppConfig)).Get<AppConfig>();
-                    if(!serviceBusConfig.IsPresent){
-                        throw new Exception("Missing Messaging configuration");
-                    }
-                    services.AddMassTransit(x =>
-                    {
-                        x.AddConsumer<BlockIPsConsumer>(typeof(BlockIPsConsumerDefinition));
-                        if (serviceBusConfig.IsPresent)
+                        x.UsingAzureServiceBus((messageContext, cfg) =>
                         {
-                            x.UsingAzureServiceBus((messageContext, cfg) =>
-                            {
-                                cfg.Host(serviceBusConfig.ConnectionString);
-                                cfg.SubscriptionEndpoint(appConfig.SiteName, "ti-blocker", e => {
-                                    e.ConfigureConsumer<BlockIPsConsumer>(messageContext);
-                                });
-                                cfg.Message<BlockIPs>(m => {
-                                    m.SetEntityName("ti-blocker");
-                                });
+                            cfg.Host(serviceBusConfig.ConnectionString);
+                            cfg.SubscriptionEndpoint(appConfig.SiteName, "ti-blocker", e => {
+                                e.ConfigureConsumer<BlockIPsConsumer>(messageContext);
                             });
-                        }
-                        else{
-                            throw new Exception("No valid Messaging configured!");
-                        }
-                        //Future add rabbitMQ config
-                    });
-                })
-                .Build()
-                .RunAsync();
+                            cfg.Message<BlockIPs>(m => {
+                                m.SetEntityName("ti-blocker");
+                            });
+                        });
+                    }
+                    else{
+                        throw new Exception("No valid Messaging configured!");
+                    }
+                    //Future add rabbitMQ config
+                });
+            })
+            .Build()
+            .RunAsync();
         }
     }
 }
