a9a1cd6baae5d57267b4dd2c13d4f64cc628c3cf
Ruben van Osch <soc@it-pg.nl>
2022-11-03 11:05:14 +0100

    Added events

---
+++ SonicWallInterface/Helpers/Events.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace SonicWallInterface.Helpers
+{
+    public static class Events
+    {
+        public static EventId Read = new EventId(1001, "Read");
+        public static EventId Error = new EventId(3001, "Error");
+    }
+}
