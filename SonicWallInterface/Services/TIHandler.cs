692a793d44dd3d7ac7c09840825cd8ac736f305a
Ruben van Osch <ruben.van.osch@it-pg.nl>
2023-07-10 14:55:27 +0200

    Implemented ASP.NET with Kestrel

--- SonicWallInterface/Services/TIHandler.cs
+++ SonicWallInterface/Services/TIHandler.cs
@@ -9,35 +9,47 @@ namespace SonicWallInterface.Services
     public class TIHandler : ITIHandler
     {
         private readonly ILogger<TIHandler> _logger;
-        private readonly ISonicWallApi _sonic;
+        private readonly IFireWallApi _fireWall;
         private readonly IThreatIntelApi _threat;
+        private readonly IHttpIPListApi _httpIps;
 
-        public TIHandler(ILogger<TIHandler> logger, ISonicWallApi sonic, IThreatIntelApi threat)
+        public TIHandler(ILogger<TIHandler> logger, IFireWallApi fireWall, IThreatIntelApi threat, IHttpIPListApi httpIps)
         {
             _logger = logger;
-            _sonic = sonic;
+            _fireWall = fireWall;
             _threat = threat;
+            _httpIps = httpIps;
+        }
+        public TIHandler(ILogger<TIHandler> logger, IThreatIntelApi threat, IHttpIPListApi httpIps)
+        {
+            _logger = logger;
+            _fireWall = null;
+            _threat = threat;
+            _httpIps = httpIps;
         }
 
         public async Task HandleTI()
         {
             _logger.Log(LogLevel.Debug, "Gathering TI from Sentinel");
             var getTiTask = _threat.GetCurrentTIIPs();
-            var currentIps = await _sonic.GetIPBlockList();
-            if(currentIps.Count <= 0)
+            var currentIps = _fireWall != null ? await _fireWall.GetIPBlockList() : new List<string>();
+            var tiIps = await getTiTask;
+            _httpIps.OverwriteIPBlockList(tiIps);
+            if (_fireWall != null)
             {
-                _logger.Log(LogLevel.Debug, "Initializing block list.");
-                var tiIps = await getTiTask;
-                await _sonic.InitiateIPBlockList(tiIps);
-            }
-            else
-            {
-                _logger.Log(LogLevel.Debug, "BlockList exists. Updating block list.");
-                var tiIps = await getTiTask;
-                var removeIps = currentIps.Where(ip => !tiIps.Contains(ip)).ToList();
-                var addIps = tiIps.Where(ip => !currentIps.Contains(ip)).ToList();
-                await _sonic.RemoveFromIPBlockList(removeIps);
-                await _sonic.AddToIPBlockList(addIps);
+                if (currentIps.Count <= 0)
+                {
+                    _logger.Log(LogLevel.Debug, "Initializing block list.");
+                    await _fireWall.InitiateIPBlockList(tiIps);
+                }
+                else
+                {
+                    _logger.Log(LogLevel.Debug, "BlockList exists. Updating block list.");
+                    var removeIps = currentIps.Where(ip => !tiIps.Contains(ip)).ToList();
+                    var addIps = tiIps.Where(ip => !currentIps.Contains(ip)).ToList();
+                    await _fireWall.RemoveFromIPBlockList(removeIps);
+                    await _fireWall.AddToIPBlockList(addIps);
+                }
             }
         }
     }
